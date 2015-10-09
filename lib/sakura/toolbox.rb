791d7568db1616994c4bb87246d9853dd9fdac4c
Masaru Ishimoto <ismtmsr@gmail.com>
2015-10-09 12:56:46 +0900

    Initial commit

---
+++ lib/sakura/toolbox.rb
@@ -0,0 +1,7 @@
+require "sakura/toolbox/version"
+
+module Sakura
+  module Toolbox
+    # Your code goes here...
+  end
+end
---
+++ spec/sakura/toolbox_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Sakura::Toolbox do
+  it 'has a version number' do
+    expect(Sakura::Toolbox::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
